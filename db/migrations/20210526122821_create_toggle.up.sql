baef1a49c96042555912ec29bb94602c7638e331
Indra Saputra <flame.fire99@gmail.com>
2021-05-26 12:38:33 +0700

    build: use docker compose to spin up postgres

--- db/migrations/20210526122821_create_toggle.up.sql
+++ db/migrations/20210526122821_create_toggle.up.sql
@@ -3,6 +3,7 @@ BEGIN;
 CREATE TABLE IF NOT EXISTS toggles (
   id            BIGSERIAL       PRIMARY KEY,
   key           TEXT            UNIQUE NOT NULL,
+  is_enabled    BOOLEAN,
   description   TEXT,
   created_at    TIMESTAMP,
   updated_at    TIMESTAMP

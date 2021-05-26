d7e704327148b37c72231ba1ef8e2e6052ff58e8
Indra Saputra <flame.fire99@gmail.com>
2021-05-26 11:49:25 +0700

    feat: service to create toggle

---
+++ service/doc.go
@@ -0,0 +1,3 @@
+// Package service defines the business logic of the requirement.
+// The general flow of the requirements are explicitly stated in the code.
+package service
---
+++ service/toggle_creator.go
@@ -0,0 +1,72 @@
+package service
+
+import (
+	"context"
+	"regexp"
+	"strings"
+
+	"github.com/indrasaputra/toggle/entity"
+)
+
+var (
+	regexCompiler *regexp.Regexp
+)
+
+func init() {
+	regexCompiler = regexp.MustCompile(`^[a-z0-9\-]+$`)
+}
+
+// CreateToggle defines the interface to create a toggle.
+type CreateToggle interface {
+	// Create creates a new toggle and store it in the storage.
+	// It must check the uniqueness of the toggle's key.
+	Create(ctx context.Context, toggle *entity.Toggle) error
+}
+
+// CreateToggleRepository defines the interface to save a toggle into the repository.
+type CreateToggleRepository interface {
+	// Insert inserts the toggle into the repository.
+	// It also validates if the toggle's key is unique.
+	Insert(ctx context.Context, toggle *entity.Toggle) error
+}
+
+// ToggleCreator is responsible for creating a new toggle.
+type ToggleCreator struct {
+	repo CreateToggleRepository
+}
+
+// NewToggleCreator creates an instance of ToggleCreator.
+func NewToggleCreator(repo CreateToggleRepository) *ToggleCreator {
+	return &ToggleCreator{repo: repo}
+}
+
+// Create creates a new toggle.
+// It will reject if the toggle's key already exists in the system.
+// To standardize the key, it converts all non-alphanumeric characters to underscore.
+func (tc *ToggleCreator) Create(ctx context.Context, toggle *entity.Toggle) error {
+	sanitizeToggle(toggle)
+	if err := validateToggle(toggle); err != nil {
+		return err
+	}
+	toggle.IsEnabled = false
+
+	return tc.repo.Insert(ctx, toggle)
+}
+
+func sanitizeToggle(toggle *entity.Toggle) {
+	toggle.Key = strings.TrimSpace(toggle.Key)
+	toggle.Key = strings.ToLower(toggle.Key)
+
+	toggle.Description = strings.TrimSpace(toggle.Description)
+}
+
+func validateToggle(toggle *entity.Toggle) error {
+	if toggle == nil {
+		return entity.ErrEmptyToggle()
+	}
+
+	if !regexCompiler.MatchString(toggle.Key) {
+		return entity.ErrInvalidKey()
+	}
+	return nil
+}

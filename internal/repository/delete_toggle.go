8e90d64b7797ec7d9b6ac94f3b16926a1ac1cebe
Indra Saputra <flame.fire99@gmail.com>
2021-05-30 11:56:48 +0700

    feat: implement repo to delete toggle

---
+++ internal/repository/delete_toggle.go
@@ -0,0 +1,58 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/indrasaputra/toggle/entity"
+)
+
+// DeleteToggleDatabase defines the interface to delete toggle from database.
+type DeleteToggleDatabase interface {
+	// GetByKey gets a toggle from database.
+	// It must return codes.NotFound from package package google.golang.org/grpc/codes if data can't be found.
+	GetByKey(ctx context.Context, key string) (*entity.Toggle, error)
+	// Delete deletes a toggle from database.
+	// It doesn't return any error if toggle is not found.
+	Delete(ctx context.Context, key string) error
+}
+
+// DeleteToggleCache defines the interface to delete a toggle in cache.
+type DeleteToggleCache interface {
+	// Delete deletes a toggle from database.
+	// It doesn't return any error if toggle is not found.
+	Delete(ctx context.Context, key string) error
+}
+
+// ToggleDeleter is responsible to delete the toggle from storage.
+// It uses database and cache.
+type ToggleDeleter struct {
+	database DeleteToggleDatabase
+	cache    DeleteToggleCache
+}
+
+// NewToggleDeleter creates an instance of ToggleDeleter.
+func NewToggleDeleter(database DeleteToggleDatabase, cache DeleteToggleCache) *ToggleDeleter {
+	return &ToggleDeleter{database: database, cache: cache}
+}
+
+// GetByKey gets the toggle from the storage.
+// It accessess the database directly without checking the cache.
+func (td *ToggleDeleter) GetByKey(ctx context.Context, key string) (*entity.Toggle, error) {
+	toggle, err := td.database.GetByKey(ctx, key)
+	if err != nil {
+		return nil, err
+	}
+	return toggle, nil
+}
+
+// DeleteByKey deletes the toggle from the storage.
+// It doesn't return any error if toggle is not found.
+func (td *ToggleDeleter) DeleteByKey(ctx context.Context, key string) error {
+	if cerr := td.cache.Delete(ctx, key); cerr != nil {
+		return cerr
+	}
+	if derr := td.database.Delete(ctx, key); derr != nil {
+		return derr
+	}
+	return nil
+}

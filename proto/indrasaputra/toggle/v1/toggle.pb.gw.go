ab64d8268e4d16e0524c0b8a949b9fc049030cc0
Indra Saputra <flame.fire99@gmail.com>
2021-06-05 10:50:05 +0700

    api: update api

--- proto/indrasaputra/toggle/v1/toggle.pb.gw.go
+++ proto/indrasaputra/toggle/v1/toggle.pb.gw.go
@@ -39,7 +39,7 @@ func request_ToggleService_CreateToggle_0(ctx context.Context, marshaler runtime
 	if berr != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Toggle); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -56,7 +56,7 @@ func local_request_ToggleService_CreateToggle_0(ctx context.Context, marshaler r
 	if berr != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Toggle); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 

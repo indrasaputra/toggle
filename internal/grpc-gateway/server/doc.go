dc3d5de98ca341fd0050c8e73c4ef8cd70532bd8
Indra Saputra <flame.fire99@gmail.com>
2021-11-03 14:51:44 +0700

    refactor: move grpc-gateway server to grpc-gateway/server directory

---
+++ internal/grpc-gateway/server/doc.go
@@ -0,0 +1,2 @@
+// Package server provides HTTP/2 to HTTP/1.1 gateway server functionality.
+package server
---
+++ internal/grpc-gateway/server/gateway_test.go
@@ -0,0 +1,63 @@
+package server_test
+
+import (
+	"testing"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/indrasaputra/toggle/internal/grpc-gateway/server"
+)
+
+var (
+	testGrpcGatewayPort = "8081"
+)
+
+func TestNewGrpcGateway(t *testing.T) {
+	t.Run("success create rest server", func(t *testing.T) {
+		srv := server.NewGrpcGateway(testGrpcGatewayPort)
+		assert.NotNil(t, srv)
+	})
+}
+
+func TestGrpcGateway_EnablePrometheus(t *testing.T) {
+	t.Run("success enable prometheus", func(t *testing.T) {
+		srv := server.NewGrpcGateway(testGrpcGatewayPort)
+		err := srv.EnablePrometheus()
+		assert.Nil(t, err)
+	})
+}
+
+func TestGrpcGateway_EnableHealth(t *testing.T) {
+	t.Run("success enable health", func(t *testing.T) {
+		srv := server.NewGrpcGateway(testGrpcGatewayPort)
+		err := srv.EnableHealth()
+		assert.Nil(t, err)
+	})
+}
+
+func TestNewGrpcGateway_AttachService(t *testing.T) {
+	t.Run("success attach service to server", func(t *testing.T) {
+		fn := func(s *runtime.ServeMux) error {
+			return nil
+		}
+
+		srv := server.NewGrpcGateway(testGrpcGatewayPort)
+
+		assert.NotPanics(t, func() { srv.AttachService(fn) })
+	})
+}
+
+func TestNewGrpcGateway_Name(t *testing.T) {
+	t.Run("success get server's name", func(t *testing.T) {
+		srv := server.NewGrpcGateway(testGrpcGatewayPort)
+		assert.Equal(t, "grpc-gateway server", srv.Name())
+	})
+}
+
+func TestNewGrpcGateway_Port(t *testing.T) {
+	t.Run("success get server's port", func(t *testing.T) {
+		srv := server.NewGrpcGateway(testGrpcGatewayPort)
+		assert.Equal(t, testGrpcGatewayPort, srv.Port())
+	})
+}
